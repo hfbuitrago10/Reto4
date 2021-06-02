d5ad85e2e38e65d87f304e326c12e56dcac595c2
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-06-02 05:34:26 -0500

    change print menu

--- App/view.py
+++ App/view.py
@@ -47,11 +47,21 @@ def printFirstLandingPoint(analyzer):
     map = analyzer['landingpointscoords']
     lstlandingpoints = mp.keySet(map)
     key = lt.firstElement(lstlandingpoints)
-    entry = mp.get(map, key)
     value = me.getValue(mp.get(map, key))
-    print("---------- Primer punto de conexión ----------")
+    print("---------- Primer punto de conexión cargado ----------")
     print("Nombre: " + str(value[2]) + "  Identificador: " + str(key) + "  Latitud: " + str(value[0]) +
     "  Longitud: " + str(value[1]) + "\n")
+
+def printLastCountry(analyzer):
+    """
+    Imprime la información del último país cargado
+    """
+    map = analyzer['countries']
+    lstcountries = mp.keySet(map)
+    key = lt.lastElement(lstcountries)
+    value = me.getValue(mp.get(map, key))
+    print("---------- Último país cargado ----------")
+    print("País: " + str(key) + "  Población: " + str(value[1]) + "  Usuarios: " + str(value[2]) + "\n")
 
 def printStronglyConnectedVertexs(analyzer, vertexa, vertexb):
     """
@@ -74,7 +84,7 @@ def printMostConnectedLandingPoint(analyzer, landingpoint, cables):
     map = analyzer['landingpointscoords']
     key = landingpoint
     value = me.getValue(mp.get(map, key))
-    print("---------- Punto de conexión con más cables conectados ----------")
+    print("---------- Punto de conexión crítico ----------")
     print("Nombre: " + str(value[2].split(', ')[0]) + "  País: " + str(value[2].split(', ')[1]) +
     "  Identificador: " + str(landingpoint))
     print("Total cables conectados: " + str(cables) + "\n")
@@ -123,7 +133,6 @@ def printConnectedCountries(analyzer, landingpoint):
     print()
     print("Total países afectados: " + str(size) + "\n")
 
-
 # Menú de opciones
 
 def printMenu():
@@ -170,16 +179,18 @@ while True:
         print()
         print("Cargando información de las conexiones....\n")
         data = loadData(analyzer)
-        print("Total puntos de conexión: ")
-        print("Total conexiones: ")
-        print("Total países: " + "\n")
+        print("Total puntos de conexión: " + str(gr.numVertices(analyzer['connections'])))
+        print("Total conexiones: " + str(gr.numEdges(analyzer['connections'])))
+        print("Total países: " + str(mp.size(analyzer['countries']))+ "\n")
         printFirstLandingPoint(analyzer)
+        printLastCountry(analyzer)
     
     elif int(inputs[0]) == 3:
         print()
         vertexa = str(input("Ingrese punto de conexión: "))
         vertexb = str(input("Ingrese punto de conexión: "))
-        print("\nTotal clusters: " + str(controller.stronglyConnectedComponents(analyzer)))
+        print("\n---------- Clusters de conexión ----------")
+        print("Total clusters: " + str(controller.stronglyConnectedComponents(analyzer)))
         printStronglyConnectedVertexs(analyzer, vertexa, vertexb)
 
     elif int(inputs[0]) == 4:
@@ -200,7 +211,8 @@ while True:
 
     elif int(inputs[0]) == 7:
         print()
-        landingpoint = str(input("Ingrese punto de conexión: "))
+        landingpointname = str(input("Ingrese punto de conexión: "))
+        landingpoint = controller.getLandingPoint(analyzer, landingpointname)
         printConnectedCountries(analyzer, landingpoint)
 
     elif int(inputs[0]) == 8:
